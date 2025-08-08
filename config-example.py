24079939f321c39dcd2e91ced50032bb43e247fd
Chanpoe <941407617@qq.com>
2025-08-09 02:07:27 +0800

    feat: add example configuration file for account and proxy settings

---
+++ config-example.py
@@ -0,0 +1,14 @@
+ACCOUNT = '1234567890@example.com'
+PASSWORD = '1234567890'
+
+PROXY_STR = 'http://127.0.0.1:7890'
+
+PROXY = {
+    'http': 'http://127.0.0.1:7890',
+    'https': 'http://127.0.0.1:7890'
+}
+
+PATH_DOMAIN_TXT = './domain.txt'
+
+# 请将此路径修改为你的chromedriver路径
+CHROME_DRIVER_PATH = '/root/pythonfiles/muniu_auto_login/chromedriver-linux64/chromedriver'
